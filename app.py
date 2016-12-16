1abee92528282a3be63a43d187869677a7dbb312
Raymond Chow <ravingrabbit75@gmail.com>
2016-12-16 15:53:10 -0800

    Config changes for pushing to heroku

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from project import socketio
 from project import app
+import os
+
+if os.environ.get("ENV") == "production":
+	debug=False
+	app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+
+else:
+	debug=True
+	app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/space_bomber'
 
 if __name__ == "__main__":
-	socketio.run(app, port=8000)
+	socketio.run(app)
--- project/__init__.py
+++ project/__init__.py
@@ -9,17 +9,6 @@ app=Flask(__name__)
 socketio=SocketIO(app)
 
 
-if os.environ.get("ENV") == "production":
-	debug=False
-	app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
-
-else:
-	debug=True
-	app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/space_bomber'
-
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 
