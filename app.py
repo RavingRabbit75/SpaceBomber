41a73323bb3649da9b323410e80f054eb82b9180
Raymond Chow <ravingrabbit75@gmail.com>
2016-12-16 16:09:16 -0800

    Another config change commit

--- app.py
+++ app.py
@@ -2,15 +2,10 @@ from project import socketio
 from project import app
 import os
 
+debug = True
+
 if os.environ.get("ENV") == "production":
 	debug=False
-	app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('HEROKU_POSTGRESQL_TEAL_URL')
-
-else:
-	debug=True
-	app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/space_bomber'
 
 if __name__ == "__main__":
-	socketio.run(app)
+	socketio.run(app, debug=app.debug)
